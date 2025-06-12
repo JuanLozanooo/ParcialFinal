fd09b4827be1966183a635151aedc8b44f95dfe8
Juan_lozanooo <jdlozano58@ucatolica.edu.co>
2025-06-12 06:26:47 -0500

    se sube home y el modelo de users

---
+++ app/users_models.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from pydantic import validator
+
+class UsersBase(SQLModel):
+    id_vuelo: int = Field(..., ge=10, le=100)
+    nombre: str = Field(..., min_length=3, max_length=50)
+    nombre_mascota: str = Field(..., min_length=3, max_length=50)
+
+class Users(UsersBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class UsersCreate(UsersBase):
+    pass
+
+class UsersRead(UsersBase):
+    id: int
+
+class UsersUpdate(SQLModel):
+    id_vuelo: Optional[int] = Field(..., ge=10, le=100)
+    nombre: Optional[str] = Field(None, min_length=3, max_length=50)
+    nombre_mascota: Optional[str] = Field(None, min_length=3, max_length=50)
+
+    @validator('*', pre=True)
+    def skip_blank_strings(cls, v):
+        if v == "":
+            return None
+        return v
+
+class UsersResponse(SQLModel):
+    id: int
+    id_vuelo: int
+    nombre: str
+    nombre_mascota: str
+
+class DeletedUsers(UsersBase, table=True):
+    __tablename__ = "deleted_users"
+    id: Optional[int] = Field(default=None, primary_key=True)
