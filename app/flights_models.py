e4d3e678575ecc26694f3e666746e882b62e26f7
Juan_lozanooo <jdlozano58@ucatolica.edu.co>
2025-06-12 05:02:46 -0500

    se suben los modelos version inicial y se corrige pets.csv

---
+++ app/flights_models.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from pydantic import validator
+
+class FlightsBase(SQLModel):
+    origen: str = Field(..., min_length=3, max_length=50)
+    destino: str = Field(..., min_length=3, max_length=50)
+    fecha: str = Field(..., min_length=3, max_length=50)
+
+class Flights(FlightsBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class FlightsCreate(FlightsBase):
+    pass
+
+class FlightsRead(FlightsBase):
+    id: int
+
+class FlightsUpdate(SQLModel):
+    origen: Optional[str] = Field(None, min_length=3, max_length=50)
+    destino: Optional[str] = Field(None, min_length=3, max_length=50)
+    fecha: Optional[str] = Field(None, min_length=3, max_length=50)
+
+    @validator('*', pre=True)
+    def skip_blank_strings(cls, v):
+        if v == "":
+            return None
+        return v
+
+class PetsResponse(SQLModel):
+    id: int
+    origen: str
+    destino: str
+    fecha: str
---
+++ app/pets_models.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from pydantic import validator
+
+class PetsBase(SQLModel):
+    id_vuelo: int = Field(..., ge=10, le=100)
+    nombre: str = Field(..., min_length=3, max_length=50)
+    edad: int = Field(..., ge=10, le=100)
+    raza: str = Field(..., min_length=3, max_length=50)
+
+class Pets(PetsBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class PetsCreate(PetsBase):
+    pass
+
+class PetsRead(PetsBase):
+    id: int
+
+class PetsUpdate(SQLModel):
+    id_vuelo: Optional[int] = Field(..., ge=10, le=100)
+    nombre: Optional[str] = Field(None, min_length=3, max_length=50)
+    edad: Optional[int] = Field(..., ge=10, le=100)
+    raza: Optional[str] = Field(None, min_length=3, max_length=50)
+
+    @validator('*', pre=True)
+    def skip_blank_strings(cls, v):
+        if v == "":
+            return None
+        return v
+
+class PetsResponse(SQLModel):
+    id: int
+    id_vuelo: int
+    nombre: str
+    edad: int
+    raza: str
